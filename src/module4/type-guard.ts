e4ac77e8c2f7815383c05a67c09870f5a2b4fb16
jubairzaman <jubair.zaman01@gmail.com>
2023-05-23 20:37:51 +0600

    some new things updatted

--- src/module4/type-guard.ts
+++ src/module4/type-guard.ts
@@ -46,3 +46,62 @@ type adminUser = {
  const admininfo : adminUser ={ name : 'Admin Vai', role: 'Admin'}
 
 console.log( getuser(userinfo), getuser(admininfo));
+
+
+// instance of guard 
+
+
+class Animale {
+    name : string ;
+    species : string;
+
+    constructor(name : string , species : string){
+
+        this.name = name ;
+        this.species = species;  
+
+    }
+
+    makeSound (){
+        console.log('I am making sounds')
+    }
+
+}
+
+
+class dog extends Animale {
+
+    constructor (name : string , species : string){
+        super(name , species );
+    }
+
+    makeBark(){
+        console.log( 'i am barking ');
+    }
+
+}
+class cat extends Animale {
+
+    constructor (name : string , species : string){
+        super(name , species );
+    }
+
+    makeMeaw (){
+        console.log('I Am Meawing');
+    }
+}
+
+
+function getAnimel (obj: Animale ){
+    if (obj instanceof dog){
+        obj.makeBark();
+    }
+    else if(obj instanceof cat){
+        obj.makeMeaw();
+    }
+}
+
+const animel1 = new dog ('German Bhai', 'dog');
+const animel2 = new cat ( 'persian' , 'cat');
+
+
