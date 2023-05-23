60a425fc91e8e7935a611760071bf3ddeb9a03d2
jubairzaman <jubair.zaman01@gmail.com>
2023-05-23 20:44:53 +0600

    getter setter

--- src/module4/Access-Modifire.ts
+++ src/module4/Access-Modifire.ts
@@ -1,30 +1,30 @@
-class BankAccount {
-    Id: number;
-    Name: String;
-    Balance: number;
+// class BankAccount {
+//     Id: number;
+//     Name: String;
+//     Balance: number;
 
 
-    constructor(Id: number, Name: string, Balance: number) {
-        this.Id = Id;
-        this.Name = Name;
-        this.Balance = Balance;
-    }
+//     constructor(Id: number, Name: string, Balance: number) {
+//         this.Id = Id;
+//         this.Name = Name;
+//         this.Balance = Balance;
+//     }
 
-    myBalance() {
-        console.log(`My Balance Is ${this.Balance}`)
-    }
+//     myBalance() {
+//         console.log(`My Balance Is ${this.Balance}`)
+//     }
 
-    addDiposit(ammount : number){
-        this.Balance = this.Balance + ammount ;
+//     addDiposit(ammount : number){
+//         this.Balance = this.Balance + ammount ;
 
-    }
+//     }
 
 
     
-}
+// }
 
 
 
-const myAccount = new BankAccount(444, 'Persian', 0);
-console.log(myAccount);
+// const myAccount = new BankAccount(444, 'Persian', 0);
+// console.log(myAccount);
 
---
+++ src/module4/gtter-setter.ts
@@ -0,0 +1,33 @@
+// class BankAccount {
+//     Id: number;
+//     Name: String;
+//     Balance: number;
+
+
+//     constructor(Id: number, Name: string, Balance: number) {
+//         this.Id = Id;
+//         this.Name = Name;
+//         this.Balance = Balance;
+//     }
+
+//     myBalance() {
+//         console.log(`My Balance Is ${this.Balance}`)
+//     }
+
+//     addDiposit(ammount : number){
+//         this.Balance = this.Balance + ammount ;
+
+//     }
+
+
+    
+// }
+
+
+
+// const myAccount = new BankAccount(444, 'Persian', 0);
+// console.log(myAccount);
+
+// myAccount.addDiposit(30);
+// myAccount.myBalance();
+
