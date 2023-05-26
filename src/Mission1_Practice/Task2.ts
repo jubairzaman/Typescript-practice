ad78aef9eac0c3c3b403a9e9bd68ec601adacb0b
jubairzaman <jubair.zaman01@gmail.com>
2023-05-27 00:07:57 +0600

    task updated

---
+++ src/Mission1_Practice/Task2.ts
@@ -0,0 +1,9 @@
+// function  findcommon ( array1 : number [], array2 : number []): number[]{
+
+//     for (const num in array1){
+//         if (array2.includes(num)){
+
+//         }
+//     }
+
+// }
---
+++ src/Mission1_Practice/task3.ts
@@ -0,0 +1,6 @@
+interface product {
+    id: number,
+    name : string,
+    price : number,
+    category : string;
+}
